2e2a08bedb8255cefe7669485e8bcf7f1fd4f143
JAICHANGPARK <aristojeff@gmail.com>
2023-04-29 10:18:14 +0900

    Create logistics_main_page.dart

---
+++ lib/ep1501_logistics_app/view/logistics_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LogisticsMainPage extends StatefulWidget {
+  const LogisticsMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<LogisticsMainPage> createState() => _LogisticsMainPageState();
+}
+
+class _LogisticsMainPageState extends State<LogisticsMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
