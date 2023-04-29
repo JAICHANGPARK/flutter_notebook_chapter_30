8bbc0f18a9aa99db3d29cc22bd270d21bb80889d
JAICHANGPARK <aristojeff@gmail.com>
2023-04-29 10:16:07 +0900

    Update logistics_app.dart

--- lib/ep1501_logistics_app/logistics_app.dart
+++ lib/ep1501_logistics_app/logistics_app.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
 
+class LogisticsApp extends StatelessWidget {
+  const LogisticsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
