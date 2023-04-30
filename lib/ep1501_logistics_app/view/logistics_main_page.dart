d25d97c632225a2b5d3b778f981949c7580cac62
JAICHANGPARK <aristojeff@gmail.com>
2023-04-30 11:23:29 +0900

    Update logistics_main_page.dart

--- lib/ep1501_logistics_app/view/logistics_main_page.dart
+++ lib/ep1501_logistics_app/view/logistics_main_page.dart
@@ -8,10 +8,12 @@ class LogisticsMainPage extends StatefulWidget {
 }
 
 class _LogisticsMainPageState extends State<LogisticsMainPage> {
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(234, 234, 234, 1),
+      backgroundColor: const Color.fromRGBO(234, 234, 234, 1),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(12.0),
@@ -21,88 +23,90 @@ class _LogisticsMainPageState extends State<LogisticsMainPage> {
                 padding: const EdgeInsets.only(top: 16),
                 child: Row(
                   children: [
-                    Text(
+                    const Text(
                       "Afternoon",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 34,
                       ),
                     ),
-                    Spacer(),
+                    const Spacer(),
                     Container(
                       decoration: BoxDecoration(
                         shape: BoxShape.circle,
                         border: Border.all(),
                       ),
-                      padding: EdgeInsets.all(8),
-                      child: Icon(
+                      padding: const EdgeInsets.all(8),
+                      child: const Icon(
                         Icons.notifications_none,
                       ),
                     ),
                     Container(
-                      margin: EdgeInsets.only(left: 8),
+                      margin: const EdgeInsets.only(left: 8),
                       decoration: BoxDecoration(
                         shape: BoxShape.circle,
                         border: Border.all(),
                       ),
-                      padding: EdgeInsets.all(8),
-                      child: Icon(
+                      padding: const EdgeInsets.all(8),
+                      child: const Icon(
                         Icons.person_outline,
                       ),
                     ),
                   ],
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 32,
               ),
               Container(
                 height: 340,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   color: Colors.yellow,
                 ),
-                margin: EdgeInsets.only(bottom: 16),
+                margin: const EdgeInsets.only(bottom: 16),
               ),
               Container(
                 height: 240,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   color: Colors.orange,
                 ),
               ),
-              Spacer(),
+              const Spacer(),
               Container(
                 height: 78,
-                margin: EdgeInsets.only(bottom: 8),
+                margin: const EdgeInsets.only(bottom: 8),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(24),
                 ),
-                padding: EdgeInsets.all(6),
+                padding: const EdgeInsets.all(6),
                 child: Row(
                   children: [
                     Expanded(
                       child: Container(
                         decoration: BoxDecoration(
-                          color: Color.fromRGBO(229, 248, 0, 1),
+                          color: const Color.fromRGBO(229, 248, 0, 1),
                           borderRadius: BorderRadius.circular(16),
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Icon(
+                            const Icon(
                               Icons.home_filled,
                               size: 28,
                             ),
-                            SizedBox(
+                            const SizedBox(
                               height: 4,
                             ),
-                            Text("Home".toUpperCase(),),
+                            Text(
+                              "Home".toUpperCase(),
+                            ),
                           ],
                         ),
                       ),
                     ),
-                    Expanded(child: Placeholder()),
-                    Expanded(child: Placeholder()),
+                    const Expanded(child: Placeholder()),
+                    const Expanded(child: Placeholder()),
                   ],
                 ),
               )
