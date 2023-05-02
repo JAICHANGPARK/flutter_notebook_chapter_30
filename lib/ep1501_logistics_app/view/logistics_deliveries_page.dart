d839eb722ae72da5ca6c929e744085f6c0266bff
JAICHANGPARK <aristojeff@gmail.com>
2023-05-02 22:59:34 +0900

    Update logistics_deliveries_page.dart

--- lib/ep1501_logistics_app/view/logistics_deliveries_page.dart
+++ lib/ep1501_logistics_app/view/logistics_deliveries_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class LogisticsDeliveriesPage extends StatefulWidget {
+  const LogisticsDeliveriesPage({Key? key}) : super(key: key);
+
+  @override
+  State<LogisticsDeliveriesPage> createState() => _LogisticsDeliveriesPageState();
+}
+
+class _LogisticsDeliveriesPageState extends State<LogisticsDeliveriesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
