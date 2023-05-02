80a7c2e22cd5a3f653bb5ba12f8d98a7007efe66
JAICHANGPARK <aristojeff@gmail.com>
2023-05-02 22:58:12 +0900

    Update logistics_home_page.dart

---
+++ lib/ep1501_logistics_app/view/logistics_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LogisticsHomePage extends StatefulWidget {
+  const LogisticsHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<LogisticsHomePage> createState() => _LogisticsHomePageState();
+}
+
+class _LogisticsHomePageState extends State<LogisticsHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
