f6a6ca0bf1fc568128baee3f7477015e0f67e5c7
JAICHANGPARK <aristojeff@gmail.com>
2023-05-02 23:12:33 +0900

    Update seperator_widget.dart

--- lib/ep1501_logistics_app/view/widgets/seperator_widget.dart
+++ lib/ep1501_logistics_app/view/widgets/seperator_widget.dart
@@ -7,8 +7,22 @@ class SeparatorWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
+        final dashCount = (constraints.constrainWidth() / (2 * 10.0)).floor();
         return Flex(
           direction: Axis.horizontal,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.generate(
+            dashCount,
+            (index) => SizedBox(
+              width: 10.0,
+              height: 1,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
         );
       },
     );
