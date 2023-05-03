974e72bda272b43825ac59a6549e1a5be2bf1b84
JAICHANGPARK <aristojeff@gmail.com>
2023-05-03 18:57:59 +0900

    Create furniture_main_page.dart

---
+++ lib/ep1505_furniture_store_app/view/furniture_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FurnitureMainPage extends StatefulWidget {
+  const FurnitureMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<FurnitureMainPage> createState() => _FurnitureMainPageState();
+}
+
+class _FurnitureMainPageState extends State<FurnitureMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
