94513b33647a7809b650ba662b57984acd0593ce
JAICHANGPARK <aristojeff@gmail.com>
2023-05-03 18:56:38 +0900

    Update ep1505_main.dart

--- lib/ep1505_furniture_store_app/ep1505_main.dart
+++ lib/ep1505_furniture_store_app/ep1505_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_30/ep1505_furniture_store_app/furniture_store_app.dart';
 
 
 void main() {
-  
+  runApp(FurnitureStoreApp());
 }
