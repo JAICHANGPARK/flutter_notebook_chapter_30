ff978a6ae4a149aa1f084d41036a6b9997f90ae8
JAICHANGPARK <aristojeff@gmail.com>
2023-05-03 18:56:39 +0900

    Update furniture_store_app.dart

--- lib/ep1505_furniture_store_app/furniture_store_app.dart
+++ lib/ep1505_furniture_store_app/furniture_store_app.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-
 class FurnitureStoreApp extends StatelessWidget {
   const FurnitureStoreApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
