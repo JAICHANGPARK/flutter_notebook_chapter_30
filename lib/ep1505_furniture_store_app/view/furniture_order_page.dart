4bd09f975ccf05ee71400da73d02d29b76ef371c
JAICHANGPARK <aristojeff@gmail.com>
2023-05-05 08:15:14 +0900

    Create furniture_order_page.dart

---
+++ lib/ep1505_furniture_store_app/view/furniture_order_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FurnitureOrderPage extends StatefulWidget {
+  const FurnitureOrderPage({Key? key}) : super(key: key);
+
+  @override
+  State<FurnitureOrderPage> createState() => _FurnitureOrderPageState();
+}
+
+class _FurnitureOrderPageState extends State<FurnitureOrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
