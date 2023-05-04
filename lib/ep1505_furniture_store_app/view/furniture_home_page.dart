0b63f953887056d76812bf0cd0af22da8d2953ca
JAICHANGPARK <aristojeff@gmail.com>
2023-05-05 08:12:09 +0900

    Update furniture_home_page.dart

--- lib/ep1505_furniture_store_app/view/furniture_home_page.dart
+++ lib/ep1505_furniture_store_app/view/furniture_home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class FurnitureHomePage extends StatefulWidget {
@@ -11,6 +10,359 @@ class FurnitureHomePage extends StatefulWidget {
 class _FurnitureHomePageState extends State<FurnitureHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 42,
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.search),
+                      hintText: "Search Furniture",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.chat_outlined),
+              ),
+              const Badge(
+                child: Icon(Icons.notifications_none),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.shopping_bag_outlined,
+                ),
+              )
+            ],
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    height: 140,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Summer Sale',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const Text(
+                          "25% off for all furniture",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        const Text(
+                          "SHOP NOW",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        DotsIndicator(
+                          dotsCount: 4,
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    height: 160,
+                    decoration: const BoxDecoration(),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  CircleAvatar(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("Sofa")
+                                ],
+                              )),
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  CircleAvatar(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("Sofa")
+                                ],
+                              )),
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  CircleAvatar(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("Sofa")
+                                ],
+                              )),
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  CircleAvatar(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("Sofa")
+                                ],
+                              )),
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  CircleAvatar(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("Sofa")
+                                ],
+                              )),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  CircleAvatar(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("Sofa")
+                                ],
+                              )),
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  CircleAvatar(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("Sofa")
+                                ],
+                              )),
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  CircleAvatar(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("Sofa")
+                                ],
+                              )),
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  CircleAvatar(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("Sofa")
+                                ],
+                              )),
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  CircleAvatar(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text("Sofa")
+                                ],
+                              )),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    height: 240,
+                    decoration: const BoxDecoration(),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[200],
+                                  ),
+                                  child: Stack(
+                                    children: const [
+                                      Positioned(
+                                        left: 16,
+                                        top: 16,
+                                        child: Text(
+                                          "Small Item",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 6,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[200],
+                                  ),
+                                  child: Stack(
+                                    children: const [
+                                      Positioned(
+                                        left: 16,
+                                        top: 16,
+                                        child: Text(
+                                          "Wood Style",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 6,
+                        ),
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                            ),
+                            child: Stack(
+                              children: const [
+                                Positioned(
+                                  left: 16,
+                                  top: 16,
+                                  child: Text(
+                                    "Minimalist\nInspiration",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Products",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.black,
+                        ),
+                        child: const Text(
+                          "See all",
+                        ),
+                      )
+                    ],
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView.builder(
+                      itemCount: 10,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          width: 186,
+                          margin: const EdgeInsets.only(right: 16),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300]!,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
