df9fda82aa666e370d0cbd904d7db0a0a7592bc2
JAICHANGPARK <aristojeff@gmail.com>
2023-05-04 19:39:56 +0900

    Update furniture_main_page.dart

--- lib/ep1505_furniture_store_app/view/furniture_main_page.dart
+++ lib/ep1505_furniture_store_app/view/furniture_main_page.dart
@@ -92,6 +92,7 @@ class _FurnitureMainPageState extends State<FurnitureMainPage> {
                             Text(
                               "SHOP NOW",
                             ),
+
                           ],
                         ),
                       ),
