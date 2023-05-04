70b3859224857a8351942c4e6df26ce958c3d06f
JAICHANGPARK <aristojeff@gmail.com>
2023-05-05 08:18:08 +0900

    Update furniture_order_page.dart

--- lib/ep1505_furniture_store_app/view/furniture_order_page.dart
+++ lib/ep1505_furniture_store_app/view/furniture_order_page.dart
@@ -10,6 +10,29 @@ class FurnitureOrderPage extends StatefulWidget {
 class _FurnitureOrderPageState extends State<FurnitureOrderPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text("Order List"),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.chat_outlined,
+              ),
+            ),
+            Badge(
+              child: Icon(Icons.notifications_none),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.shopping_bag_outlined,
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
