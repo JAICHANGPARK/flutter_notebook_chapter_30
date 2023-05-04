bad1f8b461ec6398099c499cdeb4344c30223138
JAICHANGPARK <aristojeff@gmail.com>
2023-05-05 08:10:26 +0900

    Update furniture_home_page.dart

---
+++ lib/ep1505_furniture_store_app/view/furniture_home_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class FurnitureHomePage extends StatefulWidget {
+  const FurnitureHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<FurnitureHomePage> createState() => _FurnitureHomePageState();
+}
+
+class _FurnitureHomePageState extends State<FurnitureHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
