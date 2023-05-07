15397e59634731f6672f0dd8df24cd11fcdce814
JAICHANGPARK <aristojeff@gmail.com>
2023-05-07 11:37:08 +0900

    Create farmers_app.dart

---
+++ lib/ep1509_farmers_app/view/farmers_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FarmersApp extends StatelessWidget {
+  const FarmersApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
