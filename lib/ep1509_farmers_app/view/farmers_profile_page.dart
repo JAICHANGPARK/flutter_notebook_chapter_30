49c45e19f639311e0fe8f695057c4ce9846d3640
JAICHANGPARK <aristojeff@gmail.com>
2023-05-07 11:48:30 +0900

    Update farmers_profile_page.dart

--- lib/ep1509_farmers_app/view/farmers_profile_page.dart
+++ lib/ep1509_farmers_app/view/farmers_profile_page.dart
@@ -18,13 +18,20 @@ class _FarmersProfilePageState extends State<FarmersProfilePage> {
             children: [
               Container(
                 height: 250,
-
                 decoration: BoxDecoration(
                   color: Colors.blue,
                   image: DecorationImage(
                     image: NetworkImage("https://cdn.pixabay.com/photo/2016/11/14/03/50/farmer-1822530_1280.jpg"),
                     fit: BoxFit.cover,
                   ),
+                ),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_back_ios),
+                    ),
+                  ],
                 ),
               ),
               Expanded(
