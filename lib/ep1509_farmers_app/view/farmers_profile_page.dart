5ccce95a294c0c5fc762f8a75b7badd922feb7f1
JAICHANGPARK <aristojeff@gmail.com>
2023-05-07 11:51:41 +0900

    Update farmers_profile_page.dart

--- lib/ep1509_farmers_app/view/farmers_profile_page.dart
+++ lib/ep1509_farmers_app/view/farmers_profile_page.dart
@@ -14,46 +14,50 @@ class _FarmersProfilePageState extends State<FarmersProfilePage> {
       body: Stack(
         children: [
           Positioned(
-              child: Column(
-            children: [
-              Container(
-                height: 250,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  image: DecorationImage(
-                    image: NetworkImage("https://cdn.pixabay.com/photo/2016/11/14/03/50/farmer-1822530_1280.jpg"),
-                    fit: BoxFit.cover,
+            child: Column(
+              children: [
+                Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(
+                      image: NetworkImage("https://cdn.pixabay.com/photo/2016/11/14/03/50/farmer-1822530_1280.jpg"),
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                padding: EdgeInsets.only(left: 12, top: 64),
-                child: Column(
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.arrow_back_ios),
-                          color: Colors.white,
-                        ),
-                        Text(
-                          "Back",
-                          style: TextStyle(
+                  padding: EdgeInsets.only(left: 12, top: 64),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.arrow_back_ios),
                             color: Colors.white,
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                          Text(
+                            "Back",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.white,
-                ),
-              )
-            ],
-          ))
+                Expanded(
+                  child: Container(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            child: Column(),
+          )
         ],
       ),
     );
