d03d3be6204d9267fc538bd022fda13fb68df9a8
JAICHANGPARK <aristojeff@gmail.com>
2023-05-07 11:37:26 +0900

    Update farmers_app.dart

--- lib/ep1509_farmers_app/view/farmers_app.dart
+++ lib/ep1509_farmers_app/view/farmers_app.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-
 class FarmersApp extends StatelessWidget {
   const FarmersApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
