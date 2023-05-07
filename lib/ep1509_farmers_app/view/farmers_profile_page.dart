b0b6a573dcc9a5d2c9339c9fbffb360d0828fd49
JAICHANGPARK <aristojeff@gmail.com>
2023-05-07 11:41:15 +0900

    Update farmers_profile_page.dart

---
+++ lib/ep1509_farmers_app/view/farmers_profile_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FarmersProfilePage extends StatefulWidget {
+  const FarmersProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<FarmersProfilePage> createState() => _FarmersProfilePageState();
+}
+
+class _FarmersProfilePageState extends State<FarmersProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
