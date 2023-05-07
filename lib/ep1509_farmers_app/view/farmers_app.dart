b179bade83543a2b7c790531a4dd067a7318d1d0
JAICHANGPARK <aristojeff@gmail.com>
2023-05-07 11:41:17 +0900

    Update farmers_app.dart

--- lib/ep1509_farmers_app/view/farmers_app.dart
+++ lib/ep1509_farmers_app/view/farmers_app.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class FarmersApp extends StatelessWidget {
-  const FarmersApp({Key? key}) : super(key: key);
+  FarmersApp({Key? key}) : super(key: key);
+
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: "/profile",
+        builder: (context, state) => FarmersProfilePage(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp.router();
   }
 }
