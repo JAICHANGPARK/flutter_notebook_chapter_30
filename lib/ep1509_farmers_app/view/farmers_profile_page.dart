377c6793cead39cee1dcdc9dbc1b2485a007d320
JAICHANGPARK <aristojeff@gmail.com>
2023-05-08 17:25:25 +0900

    Update farmers_profile_page.dart

--- lib/ep1509_farmers_app/view/farmers_profile_page.dart
+++ lib/ep1509_farmers_app/view/farmers_profile_page.dart
@@ -82,150 +82,162 @@ class _FarmersProfilePageState extends State<FarmersProfilePage> {
                   child: Container(
                     color: Colors.white,
                     padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Container(
-                              width: 160,
-                              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-                              decoration: BoxDecoration(
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(32),
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  "Shop",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Container(
+                                width: 160,
+                                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                                decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(32),
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    "Shop",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(
-                              width: 12,
-                            ),
-                            CircleAvatar(
-                              foregroundColor: Colors.black,
-                              backgroundColor: Colors.grey[200],
-                              child: const Icon(
-                                Icons.favorite,
+                              const SizedBox(
+                                width: 12,
                               ),
-                            )
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        const Text(
-                          "Dream Walker",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        const Text(
-                          "Republic of Korea, Seoul",
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 12),
-                          child: Text(
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "12",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 8, right: 20),
-                              child: Text("members"),
-                            ),
-                            Text(
-                              "642",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 8, right: 20),
-                              child: Text("plants in GrowthByou"),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 16),
-                          child: Wrap(
-                            spacing: 8,
-                            runSpacing: 0,
-                            children: chips
-                                .map(
-                                  (e) => Chip(
-                                    backgroundColor: Colors.grey[200],
-                                    label: Text(e),
-                                  ),
-                                )
-                                .toList(),
-                          ),
-                        ),
-                        Text(
-                          "Photo",
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        Container(
-                          margin: EdgeInsets.symmetric(vertical: 8),
-                          height: 84,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                          ),
-                          child: Row(
-                            children: [
-                              Expanded(child: Placeholder()),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              Expanded(child: Placeholder()),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              Expanded(child: Placeholder()),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              Expanded(child: Placeholder()),
+                              CircleAvatar(
+                                foregroundColor: Colors.black,
+                                backgroundColor: Colors.grey[200],
+                                child: const Icon(
+                                  Icons.favorite,
+                                ),
+                              )
                             ],
                           ),
-                        ),
-                        SizedBox(
-                          width: 16,
-                        ),
-                        Text(
-                          "Way to plant",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
+                          const SizedBox(
+                            height: 16,
                           ),
-                        ),
-                      ],
+                          const Text(
+                            "Dream Walker",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          const Text(
+                            "Republic of Korea, Seoul",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            child: Text(
+                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "12",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8, right: 20),
+                                child: Text("members"),
+                              ),
+                              Text(
+                                "642",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8, right: 20),
+                                child: Text("plants in GrowthByou"),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            child: Wrap(
+                              spacing: 8,
+                              runSpacing: 0,
+                              children: chips
+                                  .map(
+                                    (e) => Chip(
+                                      backgroundColor: Colors.grey[200],
+                                      label: Text(e),
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
+                          ),
+                          Text(
+                            "Photo",
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 8),
+                            height: 84,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                            ),
+                            child: Row(
+                              children: [
+                                Expanded(child: Placeholder()),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Expanded(child: Placeholder()),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Expanded(child: Placeholder()),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Expanded(child: Placeholder()),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            "Way to plant",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            child: Text(
+                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
