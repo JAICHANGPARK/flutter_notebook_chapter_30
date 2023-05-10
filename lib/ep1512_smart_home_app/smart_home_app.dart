dee92a3a4e0074c152247caaeda1d27736b66b2b
JAICHANGPARK <aristojeff@gmail.com>
2023-05-10 18:29:32 +0900

    Create smart_home_app.dart

---
+++ lib/ep1512_smart_home_app/smart_home_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SmartHomeApp extends StatelessWidget {
+  const SmartHomeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
