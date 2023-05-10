fb4d1704830b84bf5943609faecb95ba0c6bbff3
JAICHANGPARK <aristojeff@gmail.com>
2023-05-10 18:30:47 +0900

    Update smart_home_main_page.dart

--- lib/ep1512_smart_home_app/view/smart_home_main_page.dart
+++ lib/ep1512_smart_home_app/view/smart_home_main_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class SmartHomeMainPage extends StatefulWidget {
+  const SmartHomeMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<SmartHomeMainPage> createState() => _SmartHomeMainPageState();
+}
+
+class _SmartHomeMainPageState extends State<SmartHomeMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
