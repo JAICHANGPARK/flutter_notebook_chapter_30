3e60056d2bc7cbce3b9616338d4ee761968895bd
JAICHANGPARK <aristojeff@gmail.com>
2023-05-10 18:31:39 +0900

    Update smart_home_app.dart

--- lib/ep1512_smart_home_app/smart_home_app.dart
+++ lib/ep1512_smart_home_app/smart_home_app.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'view/smart_home_main_page.dart';
+
 
 class SmartHomeApp extends StatelessWidget {
   const SmartHomeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: SmartHomeMainPage(),
+    );
   }
 }
