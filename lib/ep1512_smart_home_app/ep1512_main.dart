c500076004b68ef732f112039a1932fd0c3029d8
JAICHANGPARK <aristojeff@gmail.com>
2023-05-11 19:09:59 +0900

    Update ep1512_main.dart

--- lib/ep1512_smart_home_app/ep1512_main.dart
+++ lib/ep1512_smart_home_app/ep1512_main.dart
@@ -3,5 +3,9 @@ import 'package:flutter_notebook_chapter_30/ep1512_smart_home_app/smart_home_app
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(ProviderScope(child: SmartHomeApp()));
+  runApp(
+    ProviderScope(
+      child: SmartHomeApp(),
+    ),
+  );
 }
