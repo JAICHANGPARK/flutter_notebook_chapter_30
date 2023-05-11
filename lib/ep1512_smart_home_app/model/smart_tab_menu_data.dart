c54bfe5e902232b11ff5342dc2facd9a7b0db6a1
JAICHANGPARK <aristojeff@gmail.com>
2023-05-11 19:07:31 +0900

    Create smart_tab_menu_data.dart

---
+++ lib/ep1512_smart_home_app/model/smart_tab_menu_data.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_notebook_chapter_30/ep1512_smart_home_app/model/smart_home_tab_menu.dart';
+
+import '';
+
+List<SmartHomeTabMenu> smartHomeTabMenuItems = [
+  SmartHomeTabMenu(
+    title: "All rooms",
+    num: "14",
+  ),
+  SmartHomeTabMenu(
+    title: "Living room",
+    num: "5",
+  ),
+  SmartHomeTabMenu(
+    title: "Bathroom",
+    num: "3",
+  ),
+  SmartHomeTabMenu(
+    title: "Bedroom",
+    num: "4",
+  ),
+];
