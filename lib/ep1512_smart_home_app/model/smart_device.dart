8eaedf86b72385ba9249eb5656001e2a8fac4669
JAICHANGPARK <aristojeff@gmail.com>
2023-05-12 17:26:46 +0900

    Update smart_device.dart

--- lib/ep1512_smart_home_app/model/smart_device.dart
+++ lib/ep1512_smart_home_app/model/smart_device.dart
@@ -1,7 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-
+part 'smart_device.freezed.dart';
 
 @freezed
-class SmartDevice with _$SmartDevice{
-
+class SmartDevice with _$SmartDevice {
+  factory SmartDevice({
+    String? name,
+    String? subtitle,
+    bool? isOn,
+  }) = _SmartDevice;
 }
