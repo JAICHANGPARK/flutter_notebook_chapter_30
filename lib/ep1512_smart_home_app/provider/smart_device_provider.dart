784995d269d56cc51c623a135316576916b7f9b2
JAICHANGPARK <aristojeff@gmail.com>
2023-05-12 17:32:24 +0900

    Update smart_device_provider.dart

--- lib/ep1512_smart_home_app/provider/smart_device_provider.dart
+++ lib/ep1512_smart_home_app/provider/smart_device_provider.dart
@@ -1,10 +1,15 @@
+import 'package:flutter_notebook_chapter_30/ep1512_smart_home_app/model/smart_device.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final smartDeviceProvider = NotifierProvider();
 
-
-class SmartDeviceService extends Notifier {
+class SmartDeviceService extends Notifier<List<SmartDevice>> {
   @override
-  build() {
+  List<SmartDevice> build() {
+    // TODO: implement build
+    return [
 
+    ];
   }
+
 }
