b91e23119816bd2461602f78477ed0196740a231
JAICHANGPARK <aristojeff@gmail.com>
2023-05-13 10:36:04 +0900

    Create smart_device_detail_page.dart

---
+++ lib/ep1512_smart_home_app/view/smart_device_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SmartDeviceDetailPage extends StatefulWidget {
+  const SmartDeviceDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<SmartDeviceDetailPage> createState() => _SmartDeviceDetailPageState();
+}
+
+class _SmartDeviceDetailPageState extends State<SmartDeviceDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
