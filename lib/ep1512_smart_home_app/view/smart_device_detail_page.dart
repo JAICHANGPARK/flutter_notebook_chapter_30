e890791319df9e35405200357d6a21973eeb9907
JAICHANGPARK <aristojeff@gmail.com>
2023-05-13 10:48:01 +0900

    Update smart_device_detail_page.dart

--- lib/ep1512_smart_home_app/view/smart_device_detail_page.dart
+++ lib/ep1512_smart_home_app/view/smart_device_detail_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_30/ep1512_smart_home_app/model/smart_device.dart';
 
 class SmartDeviceDetailPage extends StatefulWidget {
-  const SmartDeviceDetailPage({Key? key}) : super(key: key);
+  const SmartDeviceDetailPage({
+    super.key,
+    required this.smartDevice,
+  });
+
+  final SmartDevice smartDevice;
 
   @override
   State<SmartDeviceDetailPage> createState() => _SmartDeviceDetailPageState();
