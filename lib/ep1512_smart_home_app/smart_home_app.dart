5d6264ee5a922a678803b9fb74b7e8f32bca325f
JAICHANGPARK <aristojeff@gmail.com>
2023-05-13 10:37:06 +0900

    Update smart_home_app.dart

--- lib/ep1512_smart_home_app/smart_home_app.dart
+++ lib/ep1512_smart_home_app/smart_home_app.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import 'view/smart_home_main_page.dart';
 
-
 class SmartHomeApp extends StatelessWidget {
-  const SmartHomeApp({Key? key}) : super(key: key);
+   SmartHomeApp({Key? key}) : super(key: key);
+
+  final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) => SmartHomeMainPage(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SmartHomeMainPage(),
+    return MaterialApp.router(
+      routerConfig: router,
+
     );
   }
 }
