ff49a30fdec224ee3f1521730e6926fc8d832009
JAICHANGPARK <aristojeff@gmail.com>
2023-05-13 10:54:39 +0900

    Update smart_device_detail_page.dart

--- lib/ep1512_smart_home_app/view/smart_device_detail_page.dart
+++ lib/ep1512_smart_home_app/view/smart_device_detail_page.dart
@@ -64,7 +64,7 @@ class _SmartDeviceDetailPageState extends State<SmartDeviceDetailPage> {
                   )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
