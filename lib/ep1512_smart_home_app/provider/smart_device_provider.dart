c89c4d21a17b81555a7405b652cc911d00b894a9
JAICHANGPARK <aristojeff@gmail.com>
2023-05-13 10:28:47 +0900

    Update smart_device_provider.dart

--- lib/ep1512_smart_home_app/provider/smart_device_provider.dart
+++ lib/ep1512_smart_home_app/provider/smart_device_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_30/ep1512_smart_home_app/model/smart_device.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,21 +13,29 @@ class SmartDeviceService extends Notifier<List<SmartDevice>> {
         name: "Television",
         subtitle: "123456 123456",
         isOn: true,
+        color: Colors.grey[200],
+        icon: Icons.tv,
       ),
       SmartDevice(
         name: "Stereo",
         subtitle: "123456 123456",
         isOn: false,
+        color: Colors.blue[50],
+        icon: Icons.speaker,
       ),
       SmartDevice(
         name: "Lamp",
         subtitle: "123456 123456",
         isOn: false,
+        color: Colors.orange[100],
+        icon: Icons.light,
       ),
       SmartDevice(
         name: "Air-conditioning",
         subtitle: "123456 123456",
         isOn: false,
+        color: Colors.grey[200],
+        icon: Icons.devices,
       ),
     ];
   }
