c5ac3249bb2c9bf5b772b573dc42bcfef73eff8d
JAICHANGPARK <aristojeff@gmail.com>
2023-05-15 17:28:21 +0900

    Create vacation_booking_app.dart

---
+++ lib/ep1517_vacation_booking_app/vacation_booking_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VacationBookingApp extends StatelessWidget {
+  const VacationBookingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
