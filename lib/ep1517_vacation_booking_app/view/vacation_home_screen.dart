ba9e7cd4af745f8291c91b9957a8a029c5bb1b0b
JAICHANGPARK <aristojeff@gmail.com>
2023-05-15 17:34:26 +0900

    Update vacation_home_screen.dart

--- lib/ep1517_vacation_booking_app/view/vacation_home_screen.dart
+++ lib/ep1517_vacation_booking_app/view/vacation_home_screen.dart
@@ -17,26 +17,29 @@ class _VacationHomeScreenState extends State<VacationHomeScreen> {
         ),
       ),
       bottomNavigationBar: BottomAppBar(
+        color: Colors.grey[200],
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               IconButton(
                 onPressed: () {},
                 icon: Icon(Icons.search),
+                color: Colors.deepOrange,
+                iconSize: 34,
               ),
               IconButton(
                 onPressed: () {},
-                icon: Icon(Icons.favorite_border),
+                icon: Icon(Icons.favorite_border),  iconSize: 34,
               ),
               IconButton(
                 onPressed: () {},
-                icon: Icon(Icons.chat_bubble_outline),
+                icon: Icon(Icons.chat_bubble_outline),  iconSize: 34,
               ),
               IconButton(
                 onPressed: () {},
-                icon: Icon(Icons.settings_outlined),
+                icon: Icon(Icons.settings_outlined),  iconSize: 34,
               ),
             ],
           ),
