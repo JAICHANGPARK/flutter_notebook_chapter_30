b22ba49f0180c948c6e6da07e2778938afad8740
JAICHANGPARK <aristojeff@gmail.com>
2023-05-15 17:30:27 +0900

    Create vacation_home_screen.dart

---
+++ lib/ep1517_vacation_booking_app/view/vacation_home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class VacationHomeScreen extends StatefulWidget {
+  const VacationHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<VacationHomeScreen> createState() => _VacationHomeScreenState();
+}
+
+class _VacationHomeScreenState extends State<VacationHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
