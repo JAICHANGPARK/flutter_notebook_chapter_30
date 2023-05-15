9eb2742bce26c5a7b3058208c7bcaea8550d893c
JAICHANGPARK <aristojeff@gmail.com>
2023-05-15 17:28:44 +0900

    Update ep1517_main.dart

--- lib/ep1517_vacation_booking_app/ep1517_main.dart
+++ lib/ep1517_vacation_booking_app/ep1517_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_30/ep1517_vacation_booking_app/vacation_booking_app.dart';
 
 void main() {
-
+  runApp(VacationBookingApp());
 }
