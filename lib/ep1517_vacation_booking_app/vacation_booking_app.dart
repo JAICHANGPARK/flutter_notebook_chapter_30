137f02b746b7dfa0382c85e863cdd5deaa85167a
JAICHANGPARK <aristojeff@gmail.com>
2023-05-15 17:30:45 +0900

    Update vacation_booking_app.dart

--- lib/ep1517_vacation_booking_app/vacation_booking_app.dart
+++ lib/ep1517_vacation_booking_app/vacation_booking_app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_30/ep1517_vacation_booking_app/view/vacation_home_screen.dart';
 
 class VacationBookingApp extends StatelessWidget {
   const VacationBookingApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: VacationHomeScreen(),
+    );
   }
 }
