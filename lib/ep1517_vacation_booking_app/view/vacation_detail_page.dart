bbf00f9ab2086dbf71b41c4269394a02ced3a0d2
JAICHANGPARK <aristojeff@gmail.com>
2023-05-17 18:49:05 +0900

    Create vacation_detail_page.dart

---
+++ lib/ep1517_vacation_booking_app/view/vacation_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class VacationDetailPage extends StatefulWidget {
+  const VacationDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<VacationDetailPage> createState() => _VacationDetailPageState();
+}
+
+class _VacationDetailPageState extends State<VacationDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
