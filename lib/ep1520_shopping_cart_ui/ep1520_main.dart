3249d065ce1168b8e9d2455e6ca0e8a53c1fc241
JAICHANGPARK <aristojeff@gmail.com>
2023-05-18 19:10:16 +0900

    Create ep1520_main.dart

---
+++ lib/ep1520_shopping_cart_ui/ep1520_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCartUI extends StatefulWidget {
+  const ShoppingCartUI({Key? key}) : super(key: key);
+
+  @override
+  State<ShoppingCartUI> createState() => _ShoppingCartUIState();
+}
+
+class _ShoppingCartUIState extends State<ShoppingCartUI> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
