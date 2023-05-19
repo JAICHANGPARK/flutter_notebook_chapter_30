beeea4240de10a1c4abe80b5ef075785235cc4c1
JAICHANGPARK <aristojeff@gmail.com>
2023-05-19 17:53:04 +0900

    Create ep1521_podcast_app.dart

---
+++ lib/ep1521_podcast_app/ep1521_podcast_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PodcastApp());
+}
+
+class PodcastApp extends StatelessWidget {
+  const PodcastApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
