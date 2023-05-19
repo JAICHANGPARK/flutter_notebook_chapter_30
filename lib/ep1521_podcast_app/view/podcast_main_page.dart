04c860fd0783946565f4aa57e56e93b0ce56ead4
JAICHANGPARK <aristojeff@gmail.com>
2023-05-19 17:57:04 +0900

    Update podcast_main_page.dart

--- lib/ep1521_podcast_app/view/podcast_main_page.dart
+++ lib/ep1521_podcast_app/view/podcast_main_page.dart
@@ -11,30 +11,32 @@ class _PodcastMainPageState extends State<PodcastMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              CircleAvatar(),
-              Column(
-                children: [
-                  Text("Dreamwalker"),
-                  Text("Enjoy your favorite podcast."),
-                ],
-              ),
-              Container(
-                height: 48,
-                width: 48,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.grey[200]!,
-                  ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const CircleAvatar(),
+                const Column(
+                  children: [
+                    Text("Dreamwalker"),
+                    Text("Enjoy your favorite podcast."),
+                  ],
                 ),
-              )
-            ],
-          )
-        ],
+                Container(
+                  height: 48,
+                  width: 48,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.grey[200]!,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
