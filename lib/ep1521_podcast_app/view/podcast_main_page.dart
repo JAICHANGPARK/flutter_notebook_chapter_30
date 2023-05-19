774f2d57f9cf43e2e48419796ebd58b95f074588
JAICHANGPARK <aristojeff@gmail.com>
2023-05-19 17:55:03 +0900

    Create podcast_main_page.dart

---
+++ lib/ep1521_podcast_app/view/podcast_main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PodcastMainPage extends StatefulWidget {
+  const PodcastMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<PodcastMainPage> createState() => _PodcastMainPageState();
+}
+
+class _PodcastMainPageState extends State<PodcastMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
