e592bfbe1fb354b92a325f6da7593b26c7aaa732
JAICHANGPARK <aristojeff@gmail.com>
2023-05-19 17:55:14 +0900

    Update ep1521_podcast_app.dart

--- lib/ep1521_podcast_app/ep1521_podcast_app.dart
+++ lib/ep1521_podcast_app/ep1521_podcast_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_30/ep1521_podcast_app/view/podcast_main_page.dart';
 
 void main() {
   runApp(PodcastApp());
@@ -9,6 +10,8 @@ class PodcastApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: PodcastMainPage(),
+    );
   }
 }
