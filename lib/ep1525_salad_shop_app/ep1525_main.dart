a66487b20098dab3f7a83589219351feefc70209
JAICHANGPARK <aristojeff@gmail.com>
2023-05-23 19:52:56 +0900

    Update ep1525_main.dart

---
+++ lib/ep1525_salad_shop_app/ep1525_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+
+}
+
+class SaladShopApp extends StatelessWidget {
+  const SaladShopApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
