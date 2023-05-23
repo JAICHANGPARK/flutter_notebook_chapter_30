936433d623af833cc6789bfc9346fe31226f7643
JAICHANGPARK <aristojeff@gmail.com>
2023-05-23 19:54:27 +0900

    Create salad_main_page.dart

---
+++ lib/ep1525_salad_shop_app/view/salad_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SaladMainPage extends StatefulWidget {
+  const SaladMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<SaladMainPage> createState() => _SaladMainPageState();
+}
+
+class _SaladMainPageState extends State<SaladMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
