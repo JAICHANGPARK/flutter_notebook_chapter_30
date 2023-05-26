64207eef2db0632067c86158ec6407c6b5cb0b7d
JAICHANGPARK <aristojeff@gmail.com>
2023-05-26 18:15:40 +0900

    Update ep1528_main.dart

--- lib/ep1528_live_shop_app/ep1528_main.dart
+++ lib/ep1528_live_shop_app/ep1528_main.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+void main() {
+
+}
+
+
+class LiveShopApp extends StatelessWidget {
+  const LiveShopApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
