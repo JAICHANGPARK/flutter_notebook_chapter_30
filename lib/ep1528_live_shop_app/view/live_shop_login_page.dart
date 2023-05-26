c4666af795101fb41b22238664fad4375d674c91
JAICHANGPARK <aristojeff@gmail.com>
2023-05-26 18:16:22 +0900

    Create live_shop_login_page.dart

---
+++ lib/ep1528_live_shop_app/view/live_shop_login_page.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+
+class LiveShopLoginPage extends StatefulWidget {
+  const LiveShopLoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LiveShopLoginPage> createState() => _LiveShopLoginPageState();
+}
+
+class _LiveShopLoginPageState extends State<LiveShopLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
