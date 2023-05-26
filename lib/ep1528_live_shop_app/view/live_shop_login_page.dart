3198d25f8aed361dbbdfd80ef2829ac5401731f4
JAICHANGPARK <aristojeff@gmail.com>
2023-05-26 18:23:11 +0900

    Update live_shop_login_page.dart

--- lib/ep1528_live_shop_app/view/live_shop_login_page.dart
+++ lib/ep1528_live_shop_app/view/live_shop_login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class LiveShopLoginPage extends StatefulWidget {
   const LiveShopLoginPage({Key? key}) : super(key: key);
@@ -8,8 +9,22 @@ class LiveShopLoginPage extends StatefulWidget {
 }
 
 class _LiveShopLoginPageState extends State<LiveShopLoginPage> {
+  VideoPlayerController? controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = VideoPlayerController.network(
+        "https://cdn.pixabay.com/vimeo/411342239/36510.mp4?width=640&hash=e5a195df7fd4f1a6a453af1eb1eabf65a3e72762");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [],
+      ),
+    );
   }
 }
