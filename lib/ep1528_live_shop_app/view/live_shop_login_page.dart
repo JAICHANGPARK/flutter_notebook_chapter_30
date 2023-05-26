ba51021f734a87150a085d84a86fe50d24693c26
JAICHANGPARK <aristojeff@gmail.com>
2023-05-26 18:25:01 +0900

    Update live_shop_login_page.dart

--- lib/ep1528_live_shop_app/view/live_shop_login_page.dart
+++ lib/ep1528_live_shop_app/view/live_shop_login_page.dart
@@ -9,21 +9,28 @@ class LiveShopLoginPage extends StatefulWidget {
 }
 
 class _LiveShopLoginPageState extends State<LiveShopLoginPage> {
-  VideoPlayerController? controller;
+  late VideoPlayerController controller;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     controller = VideoPlayerController.network(
-        "https://cdn.pixabay.com/vimeo/411342239/36510.mp4?width=640&hash=e5a195df7fd4f1a6a453af1eb1eabf65a3e72762");
+        "https://cdn.pixabay.com/vimeo/411342239/36510.mp4?width=640&hash=e5a195df7fd4f1a6a453af1eb1eabf65a3e72762")
+      ..initialize().then((value) {
+        setState(() {});
+      });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [],
+        children: [
+          Positioned(
+            child: controller.value.isInitialized ? VideoPlayer(controller) : Container(),
+          ),
+        ],
       ),
     );
   }
