77d2b9b5546682deae08352f39f211b64fede7e3
JAICHANGPARK <aristojeff@gmail.com>
2023-05-27 10:15:57 +0900

    Create live_shop_home_page.dart

---
+++ lib/ep1528_live_shop_app/view/live_shop_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LiveShopHomePage extends StatefulWidget {
+  const LiveShopHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<LiveShopHomePage> createState() => _LiveShopHomePageState();
+}
+
+class _LiveShopHomePageState extends State<LiveShopHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
