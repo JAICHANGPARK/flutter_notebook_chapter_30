617af06ba8dfa5cd429c9089114a46764fdb3aa2
JAICHANGPARK <aristojeff@gmail.com>
2023-05-27 10:20:29 +0900

    Update live_shop_home_page.dart

--- lib/ep1528_live_shop_app/view/live_shop_home_page.dart
+++ lib/ep1528_live_shop_app/view/live_shop_home_page.dart
@@ -18,15 +18,22 @@ class _LiveShopHomePageState extends State<LiveShopHomePage> {
             Row(
               children: [
                 Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.7)
-                  ),
+                  decoration:
+                      BoxDecoration(color: Colors.white.withOpacity(0.5), borderRadius: BorderRadius.circular(16)),
+                  padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
                   child: Row(
                     children: [
                       CircleAvatar(
                         radius: 16,
                       ),
-                      Text("Dream"),
+                      SizedBox(width: 4,),
+                      Text(
+                        "Dream",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(width: 8,),
                       Icon(
                         Icons.keyboard_arrow_down,
                       )
