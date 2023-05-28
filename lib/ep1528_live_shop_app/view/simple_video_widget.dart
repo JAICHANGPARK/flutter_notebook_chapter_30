83c180c260b81dc143d563ad8b66c4b764722302
JAICHANGPARK <aristojeff@gmail.com>
2023-05-28 10:32:25 +0900

    Create simple_video_widget.dart

---
+++ lib/ep1528_live_shop_app/view/simple_video_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SimpleVideoWidget extends StatefulWidget {
+  const SimpleVideoWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SimpleVideoWidget> createState() => _SimpleVideoWidgetState();
+}
+
+class _SimpleVideoWidgetState extends State<SimpleVideoWidget> {
+  late VideoPlayerController controller = VideoPlayerController.network(
+      "https://cdn.pixabay.com/vimeo/142621176/1006.mp4?width=480&hash=8f01009f7e4a37f7e41f7250e31889c9b5bfcf59");
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return
+  }
+}
