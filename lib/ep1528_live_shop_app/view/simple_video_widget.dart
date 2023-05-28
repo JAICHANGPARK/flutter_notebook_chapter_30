0907c8fcd5a88dec80e27d3a3848954e1a4fbaca
JAICHANGPARK <aristojeff@gmail.com>
2023-05-28 10:34:53 +0900

    Update simple_video_widget.dart

--- lib/ep1528_live_shop_app/view/simple_video_widget.dart
+++ lib/ep1528_live_shop_app/view/simple_video_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class SimpleVideoWidget extends StatefulWidget {
   const SimpleVideoWidget({Key? key}) : super(key: key);
@@ -10,13 +11,23 @@ class SimpleVideoWidget extends StatefulWidget {
 class _SimpleVideoWidgetState extends State<SimpleVideoWidget> {
   late VideoPlayerController controller = VideoPlayerController.network(
       "https://cdn.pixabay.com/vimeo/142621176/1006.mp4?width=480&hash=8f01009f7e4a37f7e41f7250e31889c9b5bfcf59");
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    controller.initialize().then((value) {
+      controller.play();
+      setState(() {});
+    });
   }
+
   @override
   Widget build(BuildContext context) {
-    return
+    return controller.value.isInitialized
+        ? VideoPlayer(controller)
+        : Center(
+            child: CircularProgressIndicator(),
+          );
   }
 }
