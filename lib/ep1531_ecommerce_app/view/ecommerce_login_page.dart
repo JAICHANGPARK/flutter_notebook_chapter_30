44ae84a4c9a388472e5b6a6eabd4cbab742a71d4
JAICHANGPARK <aristojeff@gmail.com>
2023-05-29 10:43:01 +0900

    Update ecommerce_login_page.dart

--- lib/ep1531_ecommerce_app/view/ecommerce_login_page.dart
+++ lib/ep1531_ecommerce_app/view/ecommerce_login_page.dart
@@ -10,6 +10,25 @@ class EcommerceLoginPage extends StatefulWidget {
 class _EcommerceLoginPageState extends State<EcommerceLoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2023/03/05/23/46/girl-7832385_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            child: Column(
+              children: [
+                Text("Fine your unique style"),
+                Text("")
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
