a4510d1421372ccd3e3fcc6f70907af527741f3a
JAICHANGPARK <aristojeff@gmail.com>
2023-05-29 10:38:50 +0900

    Create ep1531_ecommerce_app.dart

---
+++ lib/ep1531_ecommerce_app/ep1531_ecommerce_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
