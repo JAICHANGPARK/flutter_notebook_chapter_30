6b3783f1ffcb9665fafbdcf3fc76661f2f6cb79e
JAICHANGPARK <aristojeff@gmail.com>
2023-05-29 10:40:00 +0900

    Create ecommerce_login_page.dart

---
+++ lib/ep1531_ecommerce_app/view/ecommerce_login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EcommerceLoginPage extends StatefulWidget {
+  const EcommerceLoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<EcommerceLoginPage> createState() => _EcommerceLoginPageState();
+}
+
+class _EcommerceLoginPageState extends State<EcommerceLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
