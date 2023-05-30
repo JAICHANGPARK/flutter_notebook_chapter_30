9d6b31f542661427d307ad7e563c23a63c46d815
JAICHANGPARK <aristojeff@gmail.com>
2023-05-30 17:33:53 +0900

    Update ecommerce_home_page.dart

--- lib/ep1531_ecommerce_app/view/ecommerce_home_page.dart
+++ lib/ep1531_ecommerce_app/view/ecommerce_home_page.dart
@@ -8,36 +8,111 @@ class EcommerceHomePage extends StatefulWidget {
 }
 
 class _EcommerceHomePageState extends State<EcommerceHomePage> {
-  final blueColor = Color.fromRGBO(56, 130, 205, 1);
+  final blueColor = const Color.fromRGBO(56, 130, 205, 1);
+  final greyColor = Color.fromRGBO(241, 242, 242, 1);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Container(
+                height: 48,
+                color: greyColor,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: BottomAppBar(
         height: 72,
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                CircleAvatar(
+                const CircleAvatar(
                   child: Icon(Icons.home_outlined),
                   backgroundColor: Color.fromRGBO(56, 130, 205, 1),
                   foregroundColor: Colors.white,
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  "Home",
+                  style: TextStyle(fontWeight: FontWeight.bold, color: blueColor),
+                ),
+              ],
+            ),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  child: Icon(Icons.language),
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
                 ),
                 SizedBox(
                   height: 4,
                 ),
                 Text(
-                  "Home",
+                  "Explore",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    color: blueColor
+                    color: Colors.black,
                   ),
-
+                ),
+              ],
+            ),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  child: Icon(Icons.favorite_border),
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  "Wishlist",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  child: Icon(Icons.account_circle_outlined),
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  "Profile",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
               ],
             )
