1cec93f39b46cb1f2e78a82256a3c19619862372
JAICHANGPARK <aristojeff@gmail.com>
2023-05-30 17:24:56 +0900

    Create ecommerce_home_page.dart

---
+++ lib/ep1531_ecommerce_app/view/ecommerce_home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class EcommerceHomePage extends StatefulWidget {
+  const EcommerceHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<EcommerceHomePage> createState() => _EcommerceHomePageState();
+}
+
+class _EcommerceHomePageState extends State<EcommerceHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
