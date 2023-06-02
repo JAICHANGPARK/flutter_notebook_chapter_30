e0b59439919ad5e7265e99ac7d577d6bd1c53686
JAICHANGPARK <aristojeff@gmail.com>
2023-06-02 11:20:23 +0900

    Create ep1536_main.dart

---
+++ lib/ep1536_salon_app/ep1536_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SalonApp extends StatelessWidget {
+  const SalonApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
