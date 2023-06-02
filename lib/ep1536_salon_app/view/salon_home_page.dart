5a186195b0bb8ba3d0ea205d22aa095bab5e9d20
JAICHANGPARK <aristojeff@gmail.com>
2023-06-02 11:25:41 +0900

    Update salon_home_page.dart

--- lib/ep1536_salon_app/view/salon_home_page.dart
+++ lib/ep1536_salon_app/view/salon_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SalonHomePage extends StatefulWidget {
   const SalonHomePage({Key? key}) : super(key: key);
 
@@ -11,6 +10,21 @@ class SalonHomePage extends StatefulWidget {
 class _SalonHomePageState extends State<SalonHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.location_on_outlined), label: "Location"),
+          BottomNavigationBarItem(icon: Icon(Icons.calendar_month), label: "Schedule"),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
   }
 }
