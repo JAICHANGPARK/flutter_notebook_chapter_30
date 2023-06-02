531a562f2feb337db479cad0be5ba13150618b3c
JAICHANGPARK <aristojeff@gmail.com>
2023-06-02 10:48:36 +0900

    Update ecommerce_detail_page.dart

--- lib/ep1531_ecommerce_app/view/ecommerce_detail_page.dart
+++ lib/ep1531_ecommerce_app/view/ecommerce_detail_page.dart
@@ -11,6 +11,7 @@ class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
         title: Text("Product details"),
