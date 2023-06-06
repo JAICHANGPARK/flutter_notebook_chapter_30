a8b8eab6e3c1f1877ec4d67e5f9e10e75a1414eb
JAICHANGPARK <aristojeff@gmail.com>
2023-06-06 14:57:36 +0900

    Update salons_search_page.dart

--- lib/ep1536_salon_app/view/salons_search_page.dart
+++ lib/ep1536_salon_app/view/salons_search_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SalonSearchPage extends StatefulWidget {
   const SalonSearchPage({super.key});
 
@@ -16,7 +15,67 @@ class _SalonSearchPageState extends State<SalonSearchPage> {
         length: 5,
         child: Column(
           children: [
-            
+            Container(
+              height: 64,
+              child: Row(
+                children: [
+                  Hero(
+                    tag: "search_bar",
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: TextField(
+                              enabled: false,
+                              decoration: InputDecoration(
+                                icon: Icon(
+                                  Icons.search,
+                                ),
+                                border: InputBorder.none,
+                                hintText: "Search by Salons",
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.tune,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.clear))
+                ],
+              ),
+            ),
+            TabBar(
+              tabs: [
+                Tab(text: "All"),
+                Tab(text: "All"),
+                Tab(text: "All"),
+                Tab(text: "All"),
+                Tab(text: "All"),
+              ],
+            ),
+            Expanded(
+                child: TabBarView(
+              children: [
+                Container(),
+                Container(),
+                Container(),
+                Container(),
+                Container(),
+
+              ],
+            ))
           ],
         ),
       ),
