986f99c2535622862826b162e8cb57e7079b88ba
JAICHANGPARK <aristojeff@gmail.com>
2023-06-06 14:59:08 +0900

    Update salons_search_page.dart

--- lib/ep1536_salon_app/view/salons_search_page.dart
+++ lib/ep1536_salon_app/view/salons_search_page.dart
@@ -11,62 +11,67 @@ class _SalonSearchPageState extends State<SalonSearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DefaultTabController(
-        length: 5,
-        child: Column(
-          children: [
-            Container(
-              height: 64,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Hero(
-                      tag: "search_bar",
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                        child:TextField(
-                          enabled: false,
-                          decoration: InputDecoration(
-                            icon: Icon(
-                              Icons.search,
+      body: SafeArea(
+        child: DefaultTabController(
+          length: 5,
+          child: Column(
+            children: [
+              Container(
+                // height: 64,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Hero(
+                        tag: "search_bar",
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                          child: TextField(
+                            enabled: false,
+                            decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.search,
+                              ),
+                              border: InputBorder.none,
+                              hintText: "Search by Salons",
                             ),
-                            border: InputBorder.none,
-                            hintText: "Search by Salons",
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.clear))
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: Icon(Icons.clear))
+                  ],
+                ),
+              ),
+              TabBar(
+                tabs: [
+                  Tab(text: "All"),
+                  Tab(text: "All"),
+                  Tab(text: "All"),
+                  Tab(text: "All"),
+                  Tab(text: "All"),
                 ],
               ),
-            ),
-            TabBar(
-              tabs: [
-                Tab(text: "All"),
-                Tab(text: "All"),
-                Tab(text: "All"),
-                Tab(text: "All"),
-                Tab(text: "All"),
-              ],
-            ),
-            Expanded(
-                child: TabBarView(
-              children: [
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-                Container(),
-
-              ],
-            ))
-          ],
+              Expanded(
+                  child: TabBarView(
+                children: [
+                  Container(),
+                  Container(),
+                  Container(),
+                  Container(),
+                  Container(),
+                ],
+              ))
+            ],
+          ),
         ),
       ),
     );
