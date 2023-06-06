eb8af8a8e1e2dc1537a69f370f7d33127cda38a7
JAICHANGPARK <aristojeff@gmail.com>
2023-06-06 14:52:47 +0900

    Update salons_search_page.dart

---
+++ lib/ep1536_salon_app/view/salons_search_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SalonSearchPage extends StatefulWidget {
+  const SalonSearchPage({super.key});
+
+  @override
+  State<SalonSearchPage> createState() => _SalonSearchPageState();
+}
+
+class _SalonSearchPageState extends State<SalonSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
