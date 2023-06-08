ed508cfcf80461035d82a7e12a94b9488f5b541f
JAICHANGPARK <aristojeff@gmail.com>
2023-06-08 18:55:24 +0900

    Create ep1541_main.dart

---
+++ lib/ep1541_real_estate_app/ep1541_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RealEstateApp());
+}
+
+class RealEstateApp extends StatelessWidget {
+  const RealEstateApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
