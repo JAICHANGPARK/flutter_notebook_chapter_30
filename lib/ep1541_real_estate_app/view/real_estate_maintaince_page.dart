a7d3b692701563d9b1ef993122fa5616ff914445
JAICHANGPARK <aristojeff@gmail.com>
2023-06-11 09:05:49 +0900

    Create real_estate_maintaince_page.dart

---
+++ lib/ep1541_real_estate_app/view/real_estate_maintaince_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RealEstateMaintenancePage extends StatefulWidget {
+  const RealEstateMaintenancePage({super.key});
+
+  @override
+  State<RealEstateMaintenancePage> createState() => _RealEstateMaintenancePageState();
+}
+
+class _RealEstateMaintenancePageState extends State<RealEstateMaintenancePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
