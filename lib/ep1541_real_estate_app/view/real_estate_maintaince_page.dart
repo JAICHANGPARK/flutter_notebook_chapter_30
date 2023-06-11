720790deaeead90bd2e67b1f4a3c4f7faab35a49
JAICHANGPARK <aristojeff@gmail.com>
2023-06-11 09:18:47 +0900

    Update real_estate_maintaince_page.dart

--- lib/ep1541_real_estate_app/view/real_estate_maintaince_page.dart
+++ lib/ep1541_real_estate_app/view/real_estate_maintaince_page.dart
@@ -7,39 +7,78 @@ class RealEstateMaintenancePage extends StatefulWidget {
   State<RealEstateMaintenancePage> createState() => _RealEstateMaintenancePageState();
 }
 
-class _RealEstateMaintenancePageState extends State<RealEstateMaintenancePage> {
+class _RealEstateMaintenancePageState extends State<RealEstateMaintenancePage> with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Maintenance"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.build_outlined)),
+        ],
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        bottom: TabBar(
+          controller: tabController,
+          tabs: [
+            Tab(
+              text: "Active(2)",
+            ),
+            Tab(
+              text: "History(21)",
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
         children: [
-          AppBar(
-            title: Text("Maintenance"),
-            actions: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.build_outlined)),
-            ],
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black,
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  text: "Active(2)",
-                ),
-                Tab(
-                  text: "History(21)",
-                ),
-              ],
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: Colors.grey[300]!,
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text("STATUS"),
+                            Spacer(),
+                            Icon(
+                              Icons.check_circle,
+                              size: 16,
+                              color: Colors.orange,
+                            ),
+                            Text("Confirmed")
+                          ],
+                        ),
+                      ),
+                      Divider(),
+                    ],
+                  ),
+                );
+              },
+              itemCount: 10,
             ),
           ),
-          // Expanded(
-          //   child: TabBarView(
-          //     children: [
-          //       Container(),
-          //       Container(),
-          //     ],
-          //   ),
-          // )
+          Container(),
         ],
       ),
     );
