fe5c14a2e1e9a60756fa2ee03f8cbc83c15bf117
JAICHANGPARK <aristojeff@gmail.com>
2023-06-12 17:24:16 +0900

    Update real_estate_maintaince_page.dart

--- lib/ep1541_real_estate_app/view/real_estate_maintaince_page.dart
+++ lib/ep1541_real_estate_app/view/real_estate_maintaince_page.dart
@@ -21,9 +21,9 @@ class _RealEstateMaintenancePageState extends State<RealEstateMaintenancePage> w
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Maintenance"),
+        title: const Text("Maintenance"),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.build_outlined)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.build_outlined)),
         ],
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
@@ -32,10 +32,10 @@ class _RealEstateMaintenancePageState extends State<RealEstateMaintenancePage> w
           labelColor: Colors.black,
           indicatorColor: Colors.orange,
           tabs: [
-            Tab(
+            const Tab(
               text: "Active(2)",
             ),
-            Tab(
+            const Tab(
               text: "History(21)",
             ),
           ],
@@ -44,125 +44,121 @@ class _RealEstateMaintenancePageState extends State<RealEstateMaintenancePage> w
       body: TabBarView(
         controller: tabController,
         children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(
-                      color: Colors.grey[300]!,
-                    ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    color: Colors.grey[300]!,
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: [
-                            Text(
-                              "STATUS",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey[400],
-                              ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "STATUS",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[400],
                             ),
-                            Spacer(),
-                            Icon(
-                              Icons.check_circle,
-                              size: 16,
-                              color: Colors.orange,
-                            ),
-                            Text("Confirmed")
-                          ],
-                        ),
+                          ),
+                          const Spacer(),
+                          const Icon(
+                            Icons.check_circle,
+                            size: 16,
+                            color: Colors.orange,
+                          ),
+                          const Text("Confirmed")
+                        ],
                       ),
-                      Divider(),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                        child: Row(
-                          children: [
-                            Container(
-                              height: 52,
-                              width: 52,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                color: Colors.blue,
-                              ),
+                    ),
+                    const Divider(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 52,
+                            width: 52,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: Colors.blue,
                             ),
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 8),
-                                child: Text(
-                                  "Flutter Project - Title Title Title Title Title Title Title Title",
-                                  maxLines: 2,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                          ),
+                          const Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8),
+                              child: Text(
+                                "Flutter Project - Title Title Title Title Title Title Title Title",
+                                maxLines: 2,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                             ),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.blue[50],
-                                borderRadius: BorderRadius.circular(4),
-                              ),
-                              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                              child: Text(
-                                "Live",
-                                style: TextStyle(color: Colors.blue),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Divider(),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "MAINTENANCE",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey[400],
-                              ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blue[50],
+                              borderRadius: BorderRadius.circular(4),
                             ),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.grey[200],
-                                borderRadius: BorderRadius.circular(4)
-                              ),
-                              margin: EdgeInsets.only(top: 8),
-                              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.build_outlined,
-                                    size: 14,
-                                  ),
-                                  SizedBox(
-                                    width: 7,
-                                  ),
-                                  Text(
-                                    "Addressing electrical issues",
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
+                            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                            child: const Text(
+                              "Live",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          )
+                        ],
                       ),
-                      Divider(),
-                    ],
-                  ),
-                );
-              },
-              itemCount: 10,
-            ),
+                    ),
+                    const Divider(),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "MAINTENANCE",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[400],
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(color: Colors.grey[200], borderRadius: BorderRadius.circular(4)),
+                            margin: const EdgeInsets.only(top: 8),
+                            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                            child: const Row(
+                              children: [
+                                Icon(
+                                  Icons.build_outlined,
+                                  size: 14,
+                                ),
+                                SizedBox(
+                                  width: 7,
+                                ),
+                                Text(
+                                  "Addressing electrical issues",
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const Divider(),
+                  ],
+                ),
+              );
+            },
           ),
           Container(),
         ],
