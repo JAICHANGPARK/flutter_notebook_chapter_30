181e76a94ff38edb7ad5419f893d06e3ef65d58a
JAICHANGPARK <aristojeff@gmail.com>
2023-06-13 19:49:09 +0900

    Create crowdfunding_main_page.dart

---
+++ lib/ep1546_crowdfunding_app/view/crowdfunding_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CrowdfundingMainPage extends StatefulWidget {
+  const CrowdfundingMainPage({super.key});
+
+  @override
+  State<CrowdfundingMainPage> createState() => _CrowdfundingMainPageState();
+}
+
+class _CrowdfundingMainPageState extends State<CrowdfundingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
