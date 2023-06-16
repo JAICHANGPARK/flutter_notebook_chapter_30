2d56fec714cb0e329e7f61ebd5f268d1f746de39
JAICHANGPARK <aristojeff@gmail.com>
2023-06-16 18:35:02 +0900

    Update crowdfunding_profile_page.dart

--- lib/ep1546_crowdfunding_app/view/crowdfunding_profile_page.dart
+++ lib/ep1546_crowdfunding_app/view/crowdfunding_profile_page.dart
@@ -10,6 +10,12 @@ class CrowdfundingProfilePage extends StatefulWidget {
 class _CrowdfundingProfilePageState extends State<CrowdfundingProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
