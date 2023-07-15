7e9b3c15ab197986b7ef548b4f933534be96dc6f
Dreamwalker <aristojeff@gmail.com>
2023-07-15 15:08:52 +0900

    Update ep1541_main.dart

--- lib/ep1541_real_estate_app/ep1541_main.dart
+++ lib/ep1541_real_estate_app/ep1541_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_30/ep1541_real_estate_app/view/real_estate_main_page.dart';
 
 void main() {
-  runApp(RealEstateApp());
+  runApp(const RealEstateApp());
 }
 
 class RealEstateApp extends StatelessWidget {
@@ -10,7 +10,7 @@ class RealEstateApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: RealEstateMainPage(),
     );
   }
