14cf90b27a5dacde2da2af989346aee67231e561
Dreamwalker <aristojeff@gmail.com>
2023-07-15 15:09:54 +0900

    Update ecommerce_detail_page.dart

--- lib/ep1531_ecommerce_app/view/ecommerce_detail_page.dart
+++ lib/ep1531_ecommerce_app/view/ecommerce_detail_page.dart
@@ -240,7 +240,7 @@ class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
                     ),
                   ),
                 ),
-                Text(
+                const Text(
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, "),
               ],
             ),
